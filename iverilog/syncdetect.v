879089f21273da201faaac5e7dff30af7fcd4725
svo <svofski@gmail.com>
2014-08-10 04:45:25 +0400

    decent sync verified on real vector-06c
    - horizontal delay added (32)
    - vsync shortened by half
    result: frame well centered
    back porch added so blue/gray stands out above black now, but it's not
    delayed with hsync so perhaps not ideal

--- iverilog/syncdetect.v
+++ iverilog/syncdetect.v
@@ -15,8 +15,7 @@ parameter S6 = 5;
 parameter CLK = 24e6;
 parameter HSYNC_TIME = CLK * 4.7e-6;
 //parameter HSYNC_TIME = CLK * 8.7e-6; // HSYNC pulse length =  10.7uS on real Vector-06C
-//parameter BACKPORCH_TIME = CLK * 5.7e-6;
-parameter BACKPORCH_TIME = 128;
+parameter BACKPORCH_TIME = CLK * 5.7e-6;
 parameter REST_TIME = CLK * 64e-6 - (HSYNC_TIME + BACKPORCH_TIME);
 
 parameter LINE_TIME = CLK * 64e-6;
@@ -24,8 +23,9 @@ parameter LINE_TIME = CLK * 64e-6;
 //parameter VSYNC_IN_TIME = CLK * 27e-6; // Vector-06C VSYNC zero time
 parameter VSYNC_IN_TIME = HSYNC_TIME * 3;
 
-parameter VSYNC_LONG = CLK * 1430e-6; //6*64e-6;
-//parameter VSYNC_LONG = CLK * 6*64e-6 - VSYNC_IN_TIME - 256;
+//parameter VSYNC_LONG = CLK * 1490e-6; // this matches Vector-06C VSYNC but it's awfully long
+parameter VSYNC_LONG = CLK * 1490e-6 / 2;
+parameter VSYNC_DEADTIME = CLK * 1490e-6 / 2;
 
 assign threshold = r_threshold;
 assign blacklevel = r_blacklevel;
@@ -33,29 +33,27 @@ assign blacklevel = r_blacklevel;
 reg [5:0] r_threshold = 10;
 reg [5:0] r_blacklevel = 12;
 
-reg [3:0] state = S1;
 reg [15:0] timerA;
 reg [15:0] counterA;
 reg [15:0] accu;
 
+parameter HSYNC_DELAY = 32;
+reg [HSYNC_DELAY-1:0] hsync_delay;
 reg hsync_int = 1, vsync_int = 1;
-always @*
-    //hsync <= ~(thresh_bitcount == 8);
-    hsync <= hsync_int;
+always @(posedge clk) begin
+    hsync_delay <= {hsync_delay[HSYNC_DELAY-2:0], hsync_int};
+    hsync <= hsync_delay[HSYNC_DELAY-1];
+    //hsync <= hsync_int;
+end    
     
 always @*
     vsync <= vsync_int;
     
 wire sig_raw = cvbs > r_threshold;
 
-//wire negative_edge = more_zeroes;
-reg  filtered_samp = 0;
-
-
 parameter INT_LEN = 16;
 reg [7:0] thresh_bitcount = 0;
 wire more_zeroes = (thresh_bitcount < INT_LEN/2);
-wire more_ones   = (thresh_bitcount >= INT_LEN/2);
 
 reg [INT_LEN-1:0] threshaccu = 0;
 
@@ -70,17 +68,13 @@ always @(posedge clk)
         threshaccu <= {threshaccu[INT_LEN-2:0], sig_raw};
     end
     
-
-reg [7:0] vscount = 0;
-
-integer hsync_time = HSYNC_TIME;
-integer vsync_in_time = VSYNC_IN_TIME;
 integer line_time = LINE_TIME;
 integer line_minus_hsync = LINE_TIME - HSYNC_TIME;
 integer line_minus_3x_hsync = LINE_TIME - HSYNC_TIME * 3;
 
 reg [15:0] hsync_dll = 0;
 reg more_zeroes_z = 0;
+reg vsync_deadtime;
     
 always @(posedge clk) 
     if (ce) begin
@@ -95,30 +89,47 @@ always @(posedge clk)
             hsync_dll <= hsync_dll - 1'b1;
 
         if (hsync_dll == line_time) begin
+            porch <= 0;
             hsync_int <= 0;
             accu <= 0;
         end
         else if (hsync_dll == line_minus_hsync) begin
             hsync_int <= 1;
+            
+            if (vsync_int && ~vsync_deadtime) begin
+                timerA <= BACKPORCH_TIME;
+                porch <= 1;
+            end
 
             if (more_zeroes) begin
-                r_blacklevel <= (accu >> 7) + 12;
+                r_blacklevel <= (accu >> 7) + 8;
                 r_threshold <= (accu >> 7) + 8;
             end
         end
 
+        if (porch && timerA == 0)
+            porch <= 0;
+        
         more_zeroes_z <= more_zeroes;
         if (~more_zeroes_z && more_zeroes) begin
             hsync_dll <= LINE_TIME;
         end
 
-        if (vsync_int && more_zeroes && hsync_dll == line_minus_3x_hsync) begin
+        if (vsync_int && more_zeroes && hsync_dll == line_minus_3x_hsync && ~vsync_deadtime) begin
             vsync_int <= 0;
             timerA <= VSYNC_LONG;
         end
 
-        if (~vsync_int && timerA == 0) 
+        if (~vsync_int && timerA == 0 && ~vsync_deadtime) begin
+            vsync_deadtime <= 1;
+            timerA <= VSYNC_DEADTIME;
             vsync_int <= 1;
+        end
+        
+        if (vsync_deadtime && timerA == 0) begin
+            vsync_deadtime <= 0;
+        end
+        
     end
 
 endmodule
